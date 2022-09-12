c77119ae0d4ed6310a5740a555f100f4be8959a3
Hugo Tahara Menegatti <103003201+taharaLovelace@users.noreply.github.com>
2022-09-12 19:48:46 -0300

    Update testesql.py

--- Nova pasta/python/testesql.py
+++ Nova pasta/python/testesql.py
@@ -4,8 +4,8 @@ mydb = mysql.connector.connect (
     host="localhost",
     user="root",
     passwd="1234",
-    port='3306:3306',
-    database="db"
+    port="3306",
+    database="nomes"
 )
 
 cursor = mydb.cursor()
